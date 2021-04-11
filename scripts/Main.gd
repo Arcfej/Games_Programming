cb7fd026c0638da44eace6ce35935b185a485d04
Arcfej <mayer.miklos91@gmail.com>
2021-04-11 15:33:20 +0100

    Fix stage changing error, where the old scene is not deleted

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -9,9 +9,15 @@ func change_map(path, player_position):
 	call_deferred("_deferred_change_map", path, player_position)
 
 func _deferred_change_map(path, player_position):
-		# TODO optimize loading
+	if PPlayer.get_parent():
+		PPlayer.get_parent().remove_child(PPlayer)
+	if current_map:
+		current_map.free()
+	
+	# TODO optimize loading
 	current_map = load(path).instance()
 	get_tree().get_root().add_child(current_map)
 	get_tree().set_current_scene(current_map)
+	current_map.add_child(PPlayer)
 	
-	current_map.place_player_on_map(player_position)
+	current_map.set_player_position(player_position)
--- scripts/maps/map.gd
+++ scripts/maps/map.gd
@@ -7,12 +7,6 @@ var player_position = Vector2(0, 0) setget set_player_position, get_player_posit
 func _ready():
 	PPlayer.position = player_position
 
-func place_player_on_map(position):
-	if PPlayer.get_parent():
-		PPlayer.get_parent().remove_child(PPlayer)
-	add_child(PPlayer)
-	player_position = position
-
 func get_entrances():
 	return entrances
 
